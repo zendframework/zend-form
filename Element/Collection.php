354365ef2e86ee94f85b078d8d0b1059c7c2932a
Marco Pivetta <ocramius@gmail.com>
2014-04-23 15:38:02 +0000

    Importing state as of zendframework/zf2@fe1d3456979b1b9987596d2671f065b1161c70ee (1398267482)
    Automatic import via rsync

--- Element/Collection.php
+++ Element/Collection.php
@@ -543,7 +543,6 @@ class Collection extends Fieldset
     /**
      * Add a new instance of the target element
      *
-     * @param string $name
      * @return ElementInterface
      * @throws Exception\DomainException
      */
