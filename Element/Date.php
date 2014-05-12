2611571174e9daf0ecdff899d69eac210c41ec56
Marco Pivetta <ocramius@gmail.com>
2014-05-12 03:22:52 +0000

    Importing state as of zendframework/zf2@90cb259c7f5a974d878c20e3d4c4277dc31f93fc (1399864972)
    Automatic import via rsync

--- Element/Date.php
+++ Element/Date.php
@@ -10,7 +10,6 @@
 namespace Zend\Form\Element;
 
 use DateInterval;
-use DateTimezone;
 use Zend\Form\Element;
 use Zend\Form\Element\DateTime as DateTimeElement;
 use Zend\Validator\DateStep as DateStepValidator;
@@ -51,7 +50,6 @@ class Date extends DateTimeElement
         return new DateStepValidator(array(
             'format'    => $format,
             'baseValue' => $baseValue,
-            'timezone'  => new DateTimezone('UTC'),
             'step'      => new DateInterval("P{$stepValue}D"),
         ));
     }
