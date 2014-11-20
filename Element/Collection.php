c5aaf85b9fe2428322f3d42a7bafc59bc88fd817
Marco Pivetta <ocramius@gmail.com>
2014-11-20 08:56:55 +0000

    Importing state as of zendframework/zf2@e7f120ed44ef947a2acfbbadf573920bb0d9e057 (1416473815)
    Automatic import via rsync

--- Element/Collection.php
+++ Element/Collection.php
@@ -511,37 +511,21 @@ class Collection extends Fieldset
         $values = array();
 
         foreach ($this->object as $key => $value) {
-            // If a hydrator is provided, our work here is done
             if ($this->hydrator) {
                 $values[$key] = $this->hydrator->extract($value);
-                continue;
-            }
-            
-            // If the target element is a fieldset that can accept the provided value
-            // we should clone it, inject the value and extract the data
-            if ( $this->targetElement instanceof FieldsetInterface ) {
-                if ( ! $this->targetElement->allowObjectBinding($value) ) {
-                    continue;
-                }
+            } elseif ($value instanceof $this->targetElement->object) {
+                // @see https://github.com/zendframework/zf2/pull/2848
                 $targetElement = clone $this->targetElement;
-                $targetElement->setObject($value);
+                $targetElement->object = $value;
                 $values[$key] = $targetElement->extract();
                 if (!$this->createNewObjects() && $this->has($key)) {
-                    $this->get($key)->setObject($value);
+                    $fieldset = $this->get($key);
+                    if ($fieldset instanceof Fieldset && $fieldset->allowObjectBinding($value)) {
+                        $fieldset->setObject($value);
+                    }
                 }
-                continue;
-            }
-            
-            // If the target element is a non-fieldset element, just use the value
-            if ( $this->targetElement instanceof ElementInterface ) {
-                $values[$key] = $value;
-                if (!$this->createNewObjects() && $this->has($key)) {
-                    $this->get($key)->setValue($value);
-                }
-                continue;
             }
         }
-
         return $values;
     }
 
