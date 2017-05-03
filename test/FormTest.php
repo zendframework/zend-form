laminas/laminas-form

commit 82b0cdcad2be8313f78bc53eb4d775fc243281cf

Fix collection hydration problem

src/Fieldset.php
@@ -11,6 +11,7 @@ namespace Zend\Form;
 
 use Traversable;
 use Zend\Code\Reflection\ClassReflection;
+use Zend\Form\Element\Collection;
 use Zend\Hydrator;
 use Zend\Hydrator\HydratorAwareInterface;
 use Zend\Hydrator\HydratorInterface;
@@ -564,12 +565,18 @@ class Fieldset extends Element implements FieldsetInterface
         $hydrator = $this->getHydrator();
         $hydratableData = [];
 
-        foreach ($values as $name => $value) {
-            if (!$this->has($name)) {
-                continue;
+        foreach ($this->iterator as $element) {
+            $name = $element->getName();
+
+            if (!array_key_exists($name, $values)) {
+                if (!($element instanceof Collection)) {
+                    continue;
+                }
+
+                $values[$name] = [];
             }
 
-            $element = $this->iterator->get($name);
+            $value = $values[$name];
 
             if ($element instanceof FieldsetInterface && $element->allowValueBinding()) {
                 $value = $element->bindValues($value);

test/FormTest.php
@@ -377,10 +377,10 @@ class FormTest extends TestCase
 
         $fileCollection = new Element\Collection('collection');
         $fileCollection->setOptions([
-             'count' => 2,
-             'allow_add' => false,
-             'allow_remove' => false,
-             'target_element' => $file,
+            'count' => 2,
+            'allow_add' => false,
+            'allow_remove' => false,
+            'target_element' => $file,
         ]);
         $this->form->add($fileCollection);
 
@@ -2157,5 +2157,40 @@ class FormTest extends TestCase
         $inputFilterFactory = $this->form->getFormFactory()->getInputFilterFactory();
         $inputFilter = $this->form->getInputFilter();
         $this->assertSame($inputFilterFactory, $inputFilter->getFactory());
+    }
+
+    public function testShouldHydrateEmptyCollection()
+    {
+        $fieldset = new Fieldset('example');
+        $fieldset->add([
+            'type' => 'Zend\Form\Element\Collection',
+            'name' => 'foo',
+            'options' => [
+                'label' => 'InputFilterProviderFieldset',
+                'count' => 1,
+                'target_element' => [
+                    'type' => 'text'
+                ]
+            ],
+        ]);
+
+        $this->form->add($fieldset);
+        $this->form->setBaseFieldset($fieldset);
+        $this->form->setHydrator(new \Zend\Hydrator\ObjectProperty());
+
+        $object = new Entity\SimplePublicProperty();
+        $object->foo = ['item 1', 'item 2'];
+
+        $this->form->bind($object);
+
+        $this->form->setData([
+            'submit' => 'Confirm',
+            'example' => [
+                //'foo' => [] // $_POST does't have this if collection is empty
+            ]
+        ]);
+
+        $this->assertTrue($this->form->isValid());
+        $this->assertEquals([], $this->form->getObject()->foo);
     }
 }

test/Element/CollectionTest.php
@@ -527,9 +527,9 @@ class CollectionTest extends TestCase
      */
     public function testDoesNotCreateNewObjectsWhenUsingNestedCollections()
     {
-        $addressesFieldeset = new \ZendTest\Form\TestAsset\AddressFieldset();
-        $addressesFieldeset->setHydrator(new \Zend\Hydrator\ClassMethods());
-        $addressesFieldeset->remove('city');
+        $addressesFieldset = new \ZendTest\Form\TestAsset\AddressFieldset();
+        $addressesFieldset->setHydrator(new \Zend\Hydrator\ClassMethods());
+        $addressesFieldset->remove('city');
 
         $form = new Form();
         $form->setHydrator(new ObjectPropertyHydrator());
@@ -537,7 +537,7 @@ class CollectionTest extends TestCase
             'name' => 'addresses',
             'type' => 'Collection',
             'options' => [
-                'target_element' => $addressesFieldeset,
+                'target_element' => $addressesFieldset,
                 'count' => 1
             ],
         ]);

test/TestAsset/PhoneFieldset.php
@@ -12,9 +12,10 @@ namespace ZendTest\Form\TestAsset;
 use Zend\Form\Fieldset;
 use Zend\Form\Element;
 use Zend\Hydrator\ClassMethods as ClassMethodsHydrator;
+use Zend\InputFilter\InputFilterProviderInterface;
 use ZendTest\Form\TestAsset\Entity\Phone;
 
-class PhoneFieldset extends Fieldset
+class PhoneFieldset extends Fieldset implements InputFilterProviderInterface
 {
     public function __construct()
     {
@@ -30,5 +31,14 @@ class PhoneFieldset extends Fieldset
         $number->setLabel('Number')
                ->setAttribute('class', 'form-control');
         $this->add($number);
+    }
+
+    public function getInputFilterSpecification()
+    {
+        return [
+            'number' => [
+                'required' => true,
+            ]
+        ];
     }
 }

zendframework/zend-form

commit 3f89fd94ec76118c40494d8d992e290d38668949

fixed form hydration for forms with wrapped elements, broken since 82b0cdcad2be8313f78bc53eb4d775fc243281cf

src/Fieldset.php
@@ -569,8 +569,7 @@ class Fieldset extends Element implements FieldsetInterface
         $hydrator = $this->getHydrator();
         $hydratableData = [];
 
-        foreach ($this->iterator as $element) {
-            $name = $element->getName();
+        foreach ($this->iterator as $name => $element) {
 
             if ($validationGroup
                 && (! array_key_exists($name, $validationGroup) && ! in_array($name, $validationGroup))

test/FormTest.php
@@ -785,6 +785,39 @@ class FormTest extends TestCase
         ], $model->foobar);
     }
 
+    public function testBindValuesWithWrappingPopulatesBoundObject()
+    {
+        $model = new stdClass;
+        $validSet = [
+            'foo' => 'abcde',
+            'bar' => ' ALWAYS valid ',
+            'foobar' => [
+                'foo' => 'abcde',
+                'bar' => ' always VALID',
+            ],
+        ];
+        $this->populateForm();
+        $this->form->setHydrator(new Hydrator\ObjectProperty());
+		$this->form->setName('formName');
+        $this->form->setWrapElements(true);
+		$this->form->prepare();
+        $this->form->bind($model);
+        $this->form->setData($validSet);
+
+        $this->assertObjectNotHasAttribute('foo', $model);
+        $this->assertObjectNotHasAttribute('bar', $model);
+        $this->assertObjectNotHasAttribute('foobar', $model);
+
+        $this->form->isValid();
+
+        $this->assertEquals($validSet['foo'], $model->foo);
+        $this->assertEquals('always valid', $model->bar);
+        $this->assertEquals([
+            'foo' => 'abcde',
+            'bar' => 'always valid',
+        ], $model->foobar);
+    }
+
     public function testHasFactoryComposedByDefault()
     {
         $factory = $this->form->getFormFactory();